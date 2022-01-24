fa53f221beec22f8ba9e721537647580ec5546ea
Kapoter1447 <kasper.larsson.2004@gmail.com>
2022-01-24 13:34:40 +0100

    test från pc

---
+++ 5. Fält/FältÖv3Väderstationen/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FältÖv3Väderstationen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
