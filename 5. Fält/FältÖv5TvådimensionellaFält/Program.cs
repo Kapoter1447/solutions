16a1d715f358151c2ad0ec658f6f9cab50a12a0c
Kapoter1447 <kasper.larsson.2004@gmail.com>
2022-01-31 08:17:14 +0100

    teszydxufcjnkm

---
+++ 5. Fält/FältÖv5TvådimensionellaFält/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace FältÖv5TvådimensionellaFält
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int xSize = int.Parse(Console.ReadLine());
+            int ySize = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\n");
+
+            int[,] board = new int[xSize + 1, ySize + 1];
+
+            printBoard();
+
+            Console.ReadLine();
+
+
+
+
+
+
+
+            void printBoard()
+            {
+                // X
+                Console.Write("¤");
+                for (int i = 0; i < xSize; i++)
+                {
+                    Console.Write("|" + (i + 1));
+                }
+                Console.WriteLine("|");
+
+                // Y
+                for (int i = 0; i < ySize; i++)
+                {
+                    Console.WriteLine((i + 1) + "|");
+                }
+
+                Console.WriteLine("\n ------------------ \n");
+
+            }
+
+
+        }
+    }
+}
---
+++ 5. Fält/NewFältÖv5_2DFält/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+
+namespace NewFältÖv5_2DFält
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int xSize = int.Parse(Console.ReadLine());
+            int ySize = int.Parse(Console.ReadLine());
+            int[,] board = new int[xSize, ySize];
+
+            int x;
+            int y;
+
+            Random rnd = new Random();
+
+            // Se nedan för förklaring för varför =3
+
+            int rndX = rnd.Next(0, xSize);
+            int rndY = rnd.Next(0, ySize);
+
+            board[rndX,rndY] = 3;
+
+
+            // First print (without guess)
+            Console.WriteLine("\n");
+            // X
+            Console.Write("¤");
+            for (int i = 0; i < xSize; i++)
+            {
+                Console.Write("|" + i);
+            }
+            Console.WriteLine("|");
+
+            // Y
+            for (int i = 0; i < ySize; i++)
+            {
+                Console.Write(i + "|");
+                Console.WriteLine();
+            }
+            Console.WriteLine("\n ------------------ \n");
+
+
+
+            // Guessing and printing
+            while (true)
+            {
+                // Guess
+                Console.Write("X: ");
+                x = int.Parse(Console.ReadLine());
+
+                Console.Write("Y: ");
+                y = int.Parse(Console.ReadLine());
+                
+                // Ogissade = 0
+                // Gissade fel = 1
+                // Gissade rätt = 2
+                // Ställen som är rätt = 3
+                // Nära rätt
+
+                //kolla ifall rätt såfall gör en sak. om fel gör en annan
+                if (!(board[x,y] == 3))
+                {
+                    board[x, y] = 1;
+                }
+
+                    Console.WriteLine("\n");
+                    // X
+                    Console.Write("¤");
+                    for (int i = 0; i < xSize; i++)
+                    {
+                        Console.Write("|" + i);
+                    }
+                    Console.WriteLine("|");
+
+                    // Y
+                    for (int i = 0; i < ySize; i++)
+                    {
+                        Console.Write(i + "|");
+
+                        for (int i2 = 0; i2 < xSize; i2++)
+                        {
+                            if (board[i2,i] == board[x,y] && board[x, y] == 3)
+                            {
+                                Console.Write("X ");
+                                board[x, y] = 2;
+                            }
+                            else if (board[i2,i] == 2)
+                            {
+                                Console.Write("x ");
+                            }
+                            else if (board[i2, i] == 1)
+                            {
+                                Console.Write("¤ ");
+                            }
+                            else
+                            {
+                                Console.Write(". ");
+                            }
+                        }
+                        Console.WriteLine();
+
+                    }
+                    Console.WriteLine("\n ------------------ \n");
+
+
+
+
+            }
+
+        }
+    }
+}
